d2fc197f7c1b72da942454d54dcaa31d58746be4
Jonathan Gall <odajay@gmail.com>
2016-10-17 08:51:11 +0200

    Helper file added, forgetten on previous commit

---
+++ js/helper.js
@@ -0,0 +1,9 @@
+//Helper file to add own functions
+
+
+// Perfect random int via StackOverflow
+// http://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range/1527820#1527820
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}

acdce87c5acddd82ae4fb73b8ead1d8129985d72
Jonathan Gall <odajay@gmail.com>
2016-10-18 11:18:04 +0200

    Removing and adapting old comment that no longer fits with my code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
 // Enemies our player must avoid
 var Enemy = function (y) {
     'use strict';
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     // Use a random Math method to generate the speed of our ennemies via helper.js
-
     this.speed = getRandomInt(100, 300);
     this.x = 0;
     this.y = y;
@@ -19,7 +15,7 @@ var Enemy = function (y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
     // Adding logic to reset the placement of the ennemy when it is leaving the canvas or having a collision
@@ -48,9 +44,7 @@ Enemy.prototype.render = function() {
 
 
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class initialization
 
 var Player = function(x, y) {
     'use strict';
@@ -59,6 +53,7 @@ var Player = function(x, y) {
     this.x = x;
     this.y = y;
 };
+
 
 Player.prototype.update = function(dt) {
     'use strict';
@@ -96,9 +91,9 @@ Player.prototype.handleInput = function(keyPressed) {
     }
 
 };
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+// Objects instanciation.
+// Placing all enemy objects in an array called allEnemies
+// Placing the player object in a variable called player
 
 var player = new Player(200, 380);
 
@@ -200,9 +195,8 @@ Gem.prototype.update = function(dt) {
 var gem = new Gem();
 
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to the
+// Player.handleInput() method.
 document.addEventListener('keyup', function (e) {
     'use strict';
     var allowedKeys = {
